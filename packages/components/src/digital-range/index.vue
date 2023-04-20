a650c6ede9c45bbc1a4a73fd6d74fcb4a8bdb29c
liyingxuan <melody.lyx@bitsun-inc.com>
2023-04-20 13:53:23 +0800

    DigitalRange改版1

--- packages/components/src/digital-range/index.vue
+++ packages/components/src/digital-range/index.vue
@@ -2,40 +2,35 @@
 	<div class="widght_box">
 		<el-input
 			:disabled="disabled"
-			v-model="value[0]"
+			v-model="leftVal"
 			clearable
 			@change="update"
 			placeholder="0"
 			@keyup.enter="keyUpEnter"
-			@input="e => inputValue(e, 0)"
 		>
 		</el-input>
 		<div class="mid_text">-</div>
 		<el-input
 			:disabled="disabled"
-			v-model="value[1]"
+			v-model="rightVal"
 			clearable
 			@change="update"
 			placeholder="正无穷"
 			@keyup.enter="keyUpEnter"
-			@input="e => inputValue(e, 1)"
 		>
 		</el-input>
 	</div>
 </template>
 
 <script lang="ts">
-import { toRefs, reactive, defineComponent, watch, nextTick } from 'vue';
-interface stateModel {
-	value: any;
-}
+import { toRefs, reactive, defineComponent, watch, nextTick, computed } from 'vue';
 export default defineComponent({
 	name: 'DigitalRange',
 	emits: ['update:modelValue', 'change', 'input'],
 	props: {
 		modelValue: {
 			type: Array,
-			default: [],
+			default: ['', ''],
 		},
 		disabled: {
 			type: Boolean,
@@ -51,15 +46,16 @@ export default defineComponent({
 		},
 	},
 	setup(props, { emit }) {
-		let state = reactive<stateModel>({
-			value: ['', ''],
+		let state = reactive<any>({
+			leftVal: '',
+			rightVal: '',
 		});
 
-		const formatting = () => {
+		const formatting = (val: any) => {
 			// 只能输入正整数类型的字符串,并且对比大小
 			if (props.formatting === 'numberAbsRange') {
-				let oldMin = parseInt(state.value[0]);
-				let oldMax = parseInt(state.value[1]);
+				let oldMin = parseInt(val[0]);
+				let oldMax = parseInt(val[1]);
 				const newValue = [
 					isNaN(oldMin) ? '' : Math.abs(oldMin),
 					isNaN(oldMax) ? '' : Math.abs(oldMax),
@@ -69,46 +65,36 @@ export default defineComponent({
 						return item || item === 0;
 					})
 				) {
-					state.value = newValue.sort((i1: any, i2: any) => {
+					newValue.sort((i1: any, i2: any) => {
 						return i1 - i2;
 					});
-				} else {
-					state.value = newValue;
 				}
+				const [leftVal, rightVal] = newValue
+				state.leftVal = leftVal
+				setTimeout(() => {
+					state.rightVal = rightVal
+				})
 			}
 		};
 
 		const update = () => {
-			formatting();
-			nextTick();
-			emit('update:modelValue', state.value);
+			formatting([state.leftVal, state.rightVal]);
+			setTimeout(() => {
+				emit('change', [state.leftVal, state.rightVal]);
+			})
 		};
-		const inputValue = (e: any, index: number) => {
-			emit('input', e, index);
-		}
-
-		// 监听父子value的变化
-		watch(
-			() => props.modelValue,
-			(newValue) => {
-				if (newValue) {
-					state.value = newValue;
-				} else {
-					state.value = ['', ''];
-				}
-				formatting();
-				emit('change', state.value);
-			},
-			{
-				immediate: true,
-				deep: true
-			}
-		);
+		state.leftVal = computed({
+			get: () => props.modelValue[0],
+			set: (value) => emit("update:modelValue", [value, state.rightVal])
+		})
+		state.rightVal = computed({
+			get: () => props.modelValue[1],
+			set: (value) => emit("update:modelValue", [state.leftVal, value]),
+		})
 
 		return {
 			...toRefs(state),
 			update,
-			inputValue,
 		};
 	},
 });
@@ -118,9 +104,10 @@ export default defineComponent({
 .widght_box {
 	width: 100%;
 	display: flex;
+	align-items: center;
 }
 .widght_box .el-input {
-	width: 40% !important;
+	flex: 1;
 }
 .mid_text {
 	width: 20%;
